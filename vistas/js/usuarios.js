1d08692273acce1c80fb343f0cd035add7847064
victor trujillo <trujilloramosvictormiguel@gmail.com>
2020-07-20 11:51:51 -0600

    Editar usuarios 1

--- vistas/js/usuarios.js
+++ vistas/js/usuarios.js
@@ -48,3 +48,38 @@ $(".nuevaFoto").change(function(){
 
 
 })
+
+/*=============================================
+EDITAR USUARIO
+=============================================*/
+
+ $(".btnEditarUsuario").click(function(){
+                           
+  
+  var datos = new FormData();
+  datos.append("idUsuario", idUsuario);
+
+     $.ajax({
+
+     url:"ajax/usuarios.ajax.php",
+     method: "POST",
+     data: datos,
+     cache: false,
+     contentType: false,
+     processData: false,
+     dataType: "json",
+     success: function(respuesta){
+
+          $("#editarNombre").val(respuesta["nombre"]);
+          $("#editarUsuario").val(respuesta["usuario"]);
+          $("#editarPerfil").html(respuesta["perfil"]);
+      
+   
+
+      
+
+    }
+
+   });
+
+ })
--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -100,7 +100,9 @@
                     <div class="btn-group">
                         
 
-                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                        <button class="btn btn-warning btnEditarUsuario" idUsuario="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarUsuario"><i class="fa fa-pencil"></i></button>
+
+
 
                         <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                     
@@ -279,3 +281,154 @@ MODAL AGREGAR USUARIO
   </div>
 
 </div>
+
+
+<!--=====================================
+MODAL EDITAR USUARIO
+======================================-->
+
+<div id="modalEditarUsuario" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar usuario</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarNombre" name="editarNombre" value="" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL USUARIO -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+
+                <input type="text" class="form-control input-lg" id="editarUsuario" name="editarUsuario" value="" readonly>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA CONTRASEÑA -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+
+                <input type="password" class="form-control input-lg" name="editarPassword" placeholder="Escriba la nueva contraseña">
+
+                <input type="hidden" id="passwordActual" name="passwordActual">
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
+
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-users"></i></span> 
+
+                <select class="form-control input-lg" name="editarPerfil">
+                  
+                  <option value="" id="editarPerfil"></option>
+
+                  <option value="Administrador">Administrador</option>
+
+                  <option value="Especial">Especial</option>
+
+                  <option value="Vendedor">Vendedor</option>
+
+                </select>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SUBIR FOTO -->
+
+             <div class="form-group">
+              
+              <div class="panel">SUBIR FOTO</div>
+
+              <input type="file" class="nuevaFoto" name="editarFoto">
+
+              <p class="help-block">Peso máximo de la foto 2MB</p>
+
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail previsualizar" width="100px">
+
+              <input type="hidden" name="fotoActual" id="fotoActual">
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Modificar usuario</button>
+
+        </div>
+
+     <!-- <?php
+
+          $editarUsuario = new ControladorUsuarios();
+          $editarUsuario -> ctrEditarUsuario();
+
+        ?>  -->
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
