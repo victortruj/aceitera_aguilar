7a9295089bd2c7a53fbf0580ec29b0907f9e7a76
victor trujillo <trujilloramosvictormiguel@gmail.com>
2020-08-06 19:15:32 -0600

    plugins icheck

--- vistas/js/plantilla.js
+++ vistas/js/plantilla.js
@@ -39,3 +39,12 @@ $(".tablas").DataTable({
 	}
 
 });
+
+/*=============================================
+   iCheck for checkbox and radio inputs
+=============================================*/
+
+$('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+  checkboxClass: 'icheckbox_minimal-blue',
+  radioClass   : 'iradio_minimal-blue'
+})  
