bb20cc36ac7c1303869954e10e08720022526bdd
victor trujillo <trujilloramosvictormiguel@gmail.com>
2020-08-11 11:46:36 -0600

    seleccionar productos

---
+++ controladores/productos.controlador.php
@@ -0,0 +1,20 @@
+<?php
+
+class ControladorProductos{
+
+	/*=============================================
+	MOSTRAR PRODUCTOS
+	=============================================*/
+
+
+static public function ctrMostrarProductos($item, $valor){
+
+		$tabla = "productos";
+
+		$respuesta = ModeloProductos::mdlMostrarProductos($tabla, $item, $valor);
+
+		return $respuesta;
+
+	}
+
+}
--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -56,81 +56,53 @@
         </thead>
 
         <tbody>
+
+          <?php 
+
+          $item = null;
+
+          $valor = null;
+
+          $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
+
+
+           foreach ($productos as $key => $value) {
+
+            echo '<tr>
+                  <td>'.($key+1).'</td>
+                  <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                  <td>'.$value["codigo"].'</td>
+                  <td>'.$value["descripcion"].'</td>';
+
+                  $item = "id";
+                  $valor = $value["id_categoria"];
+
+                  $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+                 echo '<td>'.$categoria["categoria"].'</td>
+                  <td>'.$value["stock"].'</td>
+                  <td>'.$value["precio_compra"].'</td>
+                  <td>'.$value["precio_venta"].'</td>
+                  <td>'.$value["fecha"].'</td>
+                  <td>
+
+                    <div class="btn-group">
+                        
+                      <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                      <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+                    </div>  
+
+                  </td>
+
+                </tr>';
+
+           }
+
+
+           ?>
           
-          <tr>
-            <td>1</td>
-            <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-            <td>0001</td>
-            <td>Lorem ipsum dolor sit amet</td>
-            <td>Lorem Ipsum</td>
-            <td>20</td>
-            <td>Q 5.00</td>
-            <td>Q 10.00</td>
-            <td>2020-08-05 12:05:32</td>
-            <td>
-
-              <div class="btn-group">
-                  
-                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-              </div>  
-
-            </td>
-
-          </tr>
-
-           <tr>
-
-            <td>1</td>
-            <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-            <td>0001</td>
-            <td>Lorem ipsum dolor sit amet</td>
-            <td>Lorem Ipsum</td>
-            <td>20</td>
-            <td>Q 5.00</td>
-            <td>Q 10.00</td>
-            <td>2020-08-05 12:05:32</td>
-            <td>
-
-              <div class="btn-group">
-                  
-                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-              </div>  
-
-            </td>
-
-          </tr>
-
-          <tr>
-            <td>1</td>
-            <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-            <td>0001</td>
-            <td>Lorem ipsum dolor sit amet</td>
-            <td>Lorem Ipsum</td>
-            <td>20</td>
-            <td>Q 5.00</td>
-            <td>Q 10.00</td>
-            <td>2020-08-05 12:05:32</td>
-            <td>
-
-              <div class="btn-group">
-                  
-                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-              </div>  
-
-            </td>
-
-          </tr>
-
-
 
         </tbody>
 
