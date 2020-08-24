f6d660fa4ed260e107e1bd7ffc9bc3a3c6a66a6c
victor trujillo <trujilloramosvictormiguel@gmail.com>
2020-08-24 13:43:32 -0600

    modificacion cabezote logo:mini/normal

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -2,30 +2,18 @@
 	
 
 <!--=====================================
-LOGOTIPO
+Logotipo
 ======================================-->
 
 <a href="inicio" class="logo">
 	
   <!-- logo mini  -->
-
-	<span class="logo-mini">
-
-		<img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive" style="padding:10px"> 	
-
-
-
-	</span>
-
+  
+<span class="logo-mini"><b>I</b>A</span>
 
   <!-- logo normal  -->
 
-
-  <span class="logo-lg">
-			
-			<img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
-
-		</span>
+<span class="logo-lg"><b>Inventario Aguilar</b></span>
 
 </a>
 
