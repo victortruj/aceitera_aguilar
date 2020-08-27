591950ddf3474b7b1c2c94ae79e88c6226bfdbc2
victor trujillo <trujilloramosvictormiguel@gmail.com>
2020-08-27 02:10:14 -0600

    mod productos depurando codigo

--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -53,54 +53,6 @@
 
         </thead>
 
-      <!--   <tbody>
-
-        <?php
-
-        $item = null;
-
-        $valor = null;
-
-        $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
-
-        foreach ($productos as $key => $value) {
-          
-          echo '<tr>
-                  <td>'.($key+1).'</td>
-                  <td><img src="vistas/img/productos/default/default.png" class="img-thumbnail" width="40px"></td>
-                  <td>'.$value["codigo"].'</td>
-                  <td>'.$value["descripcion"].'</td>';
-
-                  $item = "id";
-                  $valor = $value["id_categoria"];
-
-                  $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
-
-                 echo '<td>'.$categoria["categoria"].'</td>
-                  <td>'.$value["stock"].'</td>
-                  <td>'.$value["precio_compra"].'</td>
-                  <td>'.$value["precio_venta"].'</td>
-                  <td>'.$value["fecha"].'</td>
-                  <td>
-
-                    <div class="btn-group">
-                        
-                      <button class="btn btn-warning"><i class="fa fa-pencil-square-o"></i></button>
-
-                      <button class="btn btn-danger"><i class="fa fa-times-circle-o"></i></button>
-
-                    </div>  
-
-                  </td>
-
-                </tr>';
-
-        }
-
-        ?>
-                 
-        </tbody> -->
-
        </table>
 
       </div>
