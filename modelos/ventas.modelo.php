485d17693c1e4817144b732aba3ef3bc8bf5dc10
victor trujillo <trujilloramosvictormiguel@gmail.com>
2020-09-21 15:41:40 -0600

    Mod vetas: borrar ventas

--- modelos/ventas.modelo.php
+++ modelos/ventas.modelo.php
@@ -100,4 +100,31 @@ class ModeloVentas{
 
 	}
 
+
+	/*=============================================
+	ELIMINAR VENTA
+	=============================================*/
+
+	static public function mdlEliminarVenta($tabla, $datos){
+
+		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
+
+		$stmt -> bindParam(":id", $datos, PDO::PARAM_INT);
+
+		if($stmt -> execute()){
+
+			return "ok";
+		
+		}else{
+
+			return "error";	
+
+		}
+
+		$stmt -> close();
+
+		$stmt = null;
+
+	}
+
 }
--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -683,4 +683,73 @@ $(".tablas").on("click", ".btnEditarVenta", function(){
 
 })
 
+/*=============================================
+FUNCIÓN PARA DESACTIVAR LOS BOTONES AGREGAR CUANDO EL PRODUCTO YA HABÍA SIDO SELECCIONADO EN LA CARPETA
+=============================================*/
 
+function quitarAgregarProducto(){
+
+  //Capturamos todos los id de productos que fueron elegidos en la venta
+  var idProductos = $(".quitarProducto");
+
+  //Capturamos todos los botones de agregar que aparecen en la tabla
+  var botonesTabla = $(".tablaVentas tbody button.agregarProducto");
+
+  //Recorremos en un ciclo para obtener los diferentes idProductos que fueron agregados a la venta
+  for(var i = 0; i < idProductos.length; i++){
+
+    //Capturamos los Id de los productos agregados a la venta
+    var boton = $(idProductos[i]).attr("idProducto");
+    
+    //Hacemos un recorrido por la tabla que aparece para desactivar los botones de agregar
+    for(var j = 0; j < botonesTabla.length; j ++){
+
+      if($(botonesTabla[j]).attr("idProducto") == boton){
+
+        $(botonesTabla[j]).removeClass("btn-primary agregarProducto");
+        $(botonesTabla[j]).addClass("btn-default");
+
+      }
+    }
+
+  }
+  
+}
+
+/*=============================================
+CADA VEZ QUE CARGUE LA TABLA CUANDO NAVEGAMOS EN ELLA EJECUTAR LA FUNCIÓN:
+=============================================*/
+
+$('.tablaVentas').on( 'draw.dt', function(){
+
+  quitarAgregarProducto();
+
+})
+
+
+
+/*=============================================
+BORRAR VENTA
+=============================================*/
+$(".tablas").on("click", ".btnEliminarVenta", function(){
+
+  var idVenta = $(this).attr("idVenta");
+
+  swal({
+        title: '¿Está seguro de borrar la venta?',
+        text: "¡Si no lo está puede cancelar la accíón!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Cancelar',
+        confirmButtonText: 'Si, borrar venta!'
+      }).then(function(result){
+        if (result.value) {
+          
+            window.location = "index.php?ruta=ventas&idVenta="+idVenta;
+        }
+
+  })
+
+})
